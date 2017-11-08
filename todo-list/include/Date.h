11425a11954f827a4276d8c5bd98177842cb4fc5
Dushan <dusanterzic87@gmail.com>
2017-11-08 10:42:42 +0100

    backup commit

---
+++ todo-list/include/Date.h
@@ -0,0 +1,18 @@
+#ifndef DATE_H
+#define DATE_H
+#include <iostream>
+#include <string>
+
+class Date{
+
+private:
+    string time;
+    int day, month, year;
+
+public:
+
+
+
+};
+
+#endif // DATE_H
